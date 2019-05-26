06a070a403698caff1f5dbf210377b480da02115
longtime1116 <longtime1116@gmail.com>
2019-05-26 17:29:43 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 #define REP(i,init, n) for(lli i=init;i<n;i++)
 #define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
 #define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
 
 typedef long long int lli;
