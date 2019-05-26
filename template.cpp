9a9775e2335a648a08b5b8bbb1c65a5844b509cb
longtime1116 <longtime1116@gmail.com>
2019-05-26 16:55:27 +0900

    template.cpp

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+
+int main() {
+}
