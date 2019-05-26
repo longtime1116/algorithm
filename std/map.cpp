7c8318699190c4f295867d74c58a660fe8a773fa
longtime1116 <longtime1116@gmail.com>
2019-05-26 17:40:40 +0900

    std map

---
+++ std/map.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+
+int main() {
+    map<string, int> m;
+
+    cout << "----- insert -----" << endl;
+    m.insert(make_pair("abc", 20));
+    m.insert(make_pair("bcd", 10));
+    m.insert(make_pair("azz", 30));
+
+    REPIT(it, m) {
+        printf("key: %s, value: %d\n", it->first.c_str(), it->second);
+    }
+
+    cout << "----- erase -----" << endl;
+    m.erase("bcd");
+    REPIT(it, m) {
+        printf("key: %s, value: %d\n", it->first.c_str(), it->second);
+    }
+}
