242ba32e085566af317585d1bcc0ae912f1ab0df
longtime1116 <longtime1116@gmail.com>
2019-07-08 22:27:45 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,10 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// iterator
 #define REP(i,init, n) for(lli i=init;i<n;i++)
 #define REPE(i,init, n) for(lli i=init;i<=n;i++)
 #define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
 #define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
 
 #define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
 
@@ -12,4 +21,6 @@ typedef long long int lli;
 typedef pair<lli, lli> P;
 
 int main() {
+    lli n;
+    cin1(n);
 }
