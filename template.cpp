ca8a1ac0488b73a127ce27bbe12c8b94c6dd863f
shumo.tokinaga <longtime1116@gmail.com>
2019-07-24 18:06:24 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -15,6 +15,13 @@ using namespace std;
 #define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
 #define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
 
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
 #define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
 
 typedef long long int lli;
